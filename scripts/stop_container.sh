044fe844d4c6ac9e2d0410398f29c1d5a146373a
Abdulrajak s Mulla <112795885+itsmeabd85@users.noreply.github.com>
2025-01-08 00:57:55 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 set -e
 
-# Check for running containers for the same image
+# Find and stop running containers for the application
 container_id=$(docker ps --filter "ancestor=abdulrajak/sample-app:latest" --filter "status=running" -q)
 
-# Stop and remove the running container
 if [ -n "$container_id" ]; then
     echo "Stopping container with ID: $container_id"
     docker stop "$container_id"
@@ -13,11 +12,11 @@ else
     echo "No running containers found for this image."
 fi
 
-# Free up port 5001 if it's still occupied
+# Check and free up port 5001 if occupied
 if sudo lsof -i :5001; then
-    echo "Port 5001 is still in use. Killing the process using it."
+    echo "Port 5001 is in use. Killing the process occupying it."
     pid=$(sudo lsof -ti :5001)
     sudo kill -9 "$pid"
 fi
 
-echo "Stopped and cleaned up successfully."
+echo "Stop script executed successfully."
