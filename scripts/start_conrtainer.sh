7f567e01111d6a240d9ab82a0bde2adb3218a66a
Abdulrajak s Mulla <112795885+itsmeabd85@users.noreply.github.com>
2025-01-07 23:05:14 +0530

    Update start_conrtainer.sh

--- scripts/start_conrtainer.sh
+++ scripts/start_conrtainer.sh
@@ -2,7 +2,7 @@
 set -e
 
 # pull the docker image from the docker-hub
-docker pull abdulrajak/final-app:latest
+docker pull abdulrajak/sample-app:latest
 
 # run the docker image as conatiner
-docker run -d -p 5000:8080 abdulrajak/final-app:latest
+docker run -d -p 5000:8080 abdulrajak/sample-app:latest
