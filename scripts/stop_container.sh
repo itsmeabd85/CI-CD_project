34698dcdab2808e20d71955b5e4cd13899a1f579
Abdulrajak s Mulla <112795885+itsmeabd85@users.noreply.github.com>
2025-01-08 00:47:52 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
-set -euxo pipefail
-
-# Check for running containers on port 5000
 container_id=$(docker ps --filter "ancestor=abdulrajak/sample-app:latest" --filter "status=running" -q)
-
 if [ -n "$container_id" ]; then
-    echo "Stopping and removing running container with ID: $container_id"
     docker stop "$container_id"
     docker rm "$container_id"
-else
-    echo "No running containers found for this image."
 fi
-
-# Verify port 5000 availability
-if sudo lsof -i :5001; then
-    echo "Port 5001 is still in use. Killing the process using it."
-    pid=$(sudo lsof -ti :5001)
-    sudo kill -9 "$pid"
-fi
-
-# Start the new container
-docker run -d -p 5001:5001 abdulrajak/sample-app:latest || { echo "Failed to start the container"; exit 1; }
-
-echo "Container started successfully!"
-
