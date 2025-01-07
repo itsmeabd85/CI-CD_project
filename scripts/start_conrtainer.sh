238947f232d8d1b3b28277df3ad09d0220e2eff0
Abdulrajak s Mulla <112795885+itsmeabd85@users.noreply.github.com>
2025-01-08 00:55:22 +0530

    Update start_conrtainer.sh

--- scripts/start_conrtainer.sh
+++ scripts/start_conrtainer.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
-# pull the docker image from the docker-hub
+# Pull the latest Docker image
 docker pull abdulrajak/sample-app:latest
 
-# run the docker image as conatiner
+# Stop and remove any running container on port 5001
+container_id=$(docker ps --filter "ancestor=abdulrajak/sample-app:latest" --filter "status=running" -q)
+if [ -n "$container_id" ]; then
+    docker stop "$container_id"
+    docker rm "$container_id"
+fi
+
+# Run the new container
 docker run -d -p 5001:5001 abdulrajak/sample-app:latest
